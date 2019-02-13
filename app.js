dd1b3226de4beaa14266f47bea5486e1b727b823
Martyn Stirk <startinmerc@gmail.com>
2019-02-13 14:11:51 +0000

    update URL for Heroku

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var express     = require("express"),
 
     seedDB      = require('./seeds');
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v13",{useNewUrlParser: true});
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_v13";
+
+mongoose.connect(url,{useNewUrlParser: true});
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
